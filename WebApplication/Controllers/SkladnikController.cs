aa52e268f57fdc62525da18e14c9242664100864
Gabrysia Sawicka <99sgabi@wp.pl>
2020-12-02 23:57:23 +0100

    Translation

--- WebApplication/Controllers/SkladnikController.cs
+++ WebApplication/Controllers/SkladnikController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -11,6 +12,7 @@ using WebApplication.Models;
 
 namespace WebApplication.Controllers
 {
+    [Authorize]
     public class SkladnikController : Controller
     {
         private readonly MyContext _context;
