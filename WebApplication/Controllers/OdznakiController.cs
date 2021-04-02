2807a5e44f603f2e99325f00f37e1a01d1e80bfc
Gabrysia Sawicka <99sgabi@wp.pl>
2021-04-02 18:01:42 +0200

    Odznaki mockup

--- WebApplication/Controllers/OdznakiController.cs
+++ WebApplication/Controllers/OdznakiController.cs
@@ -2,14 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WebApplication.Controllers
 {
+    [Authorize]
     public class OdznakiController : Controller
     {
         public IActionResult Index()
         {
+            int userId = int.Parse(User.Identity.GetUserId());
+            ViewBag.gold = false;
+            ViewBag.silver = true;
+            ViewBag.bronze = true;
+            ViewBag.progressLeft = 20;
+            ViewBag.progress = 80;
             return View();
         }
     }
